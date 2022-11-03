604ce203eac57dcea9c0cff3dc47a89668f2888b
gimhanul <gimhanuly@gmail.com>
2022-11-03 12:26:24 +0900

    init

---
+++ src/main/java/com/young/bbabbico/BbabbicoApplication.java
@@ -0,0 +1,13 @@
+package com.young.bbabbico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbabbicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbabbicoApplication.class, args);
+	}
+
+}
