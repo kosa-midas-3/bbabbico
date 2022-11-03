56495b3c669f68472bbd59a33e65e33f3265d92e
gimhanul <gimhanuly@gmail.com>
2022-11-04 00:03:45 +0900

    ADD :: CoreTime (#12)

---
+++ src/main/java/com/young/bbabbico/domain/coretime/domain/CoreTime.java
@@ -0,0 +1,43 @@
+package com.young.bbabbico.domain.coretime.domain;
+
+import com.young.bbabbico.domain.user.domain.User;
+import com.young.bbabbico.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Entity
+@Table(name = "tbl_core_time")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CoreTime extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public CoreTime(LocalDate date, LocalTime startTime, LocalTime endTime, User user) {
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/young/bbabbico/domain/coretime/domain/repository/CoreTimeRepository.java
@@ -0,0 +1,7 @@
+package com.young.bbabbico.domain.coretime.domain.repository;
+
+import com.young.bbabbico.domain.coretime.domain.CoreTime;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CoreTimeRepository extends CrudRepository<CoreTime, Long> {
+}
